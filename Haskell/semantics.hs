fbe96627289d925542a5e95310603a6cf48eac82
temainowu <romanstirrat@gmail.com>
2026-06-26 15:42:35 +0100

    pretty sure this is what I meant - still unfinished and likely will never be

--- Haskell/semantics.hs
+++ Haskell/semantics.hs
@@ -4,11 +4,11 @@ kase :: Int -> a -> F a b -> F a b
 kase 0 x (T f) = f x
 kase n x (T f)  = T (\a -> kase (n-1) x (f a))
 
-f :: [a] -> a -> Bool
-f xs = `elem` xs
+f :: Eq a => [a] -> a -> Bool
+f xs x = x `elem` xs
 
-a :: [a] -> a -> Bool -> (a -> Bool) -> Bool
-a qs x p = all [x q <= p q | q <- qs]
+a :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
+a qs x p = and [x q <= p q | q <- qs]
 
-e :: [a] -> a -> Bool -> (a -> Bool) -> Bool
+e :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
 e qs x p = or []
