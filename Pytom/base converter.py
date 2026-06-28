ba6070bc01b63bfbd1830da861946b29a052abc0
temainowu <romanstirrat@gmail.com>
2026-06-28 03:40:12 +0100

    trying to figure what this does - floating point error in this program is funny

--- Pytom/base converter.py
+++ Pytom/base converter.py
@@ -1,11 +1,17 @@
-num = float(input())
-base = int(input())
-i = 0
+
+num = float(input()) # input number in base ten
+base = int(input()) # input base to convert to (in base ten)
+
+i = 0 # initialize counter and output string
 out = ''
 
 while int(num) > 0:
-    num /= base
+    num /= base 
     i += 1
+
+# assuming positive input for num
+# i is now the least positive integer such that num / (base ^ i) < 1
+# num is now num / (base ^ i)
 
 for _ in range(i):
     num *= base
